ce3380341aae9815ff846612a23bdc8a0980a290
Jabier Espinal <jabierespinal@gmail.com>
2017-11-29 16:39:25 +0000

    news api coding second api

--- index.js
+++ index.js
@@ -7,20 +7,34 @@ $(document).ready(function() {
         success: function(data){
           if (data.status === "ok")  {
               console.log(data)
-              for (var i = 0; i < data.sources.length; i++) {
+              for (var i = 0; i < data.sources.length; i++) { //for loop data sources
                     var source = document.createElement("OPTION");
                     source.setAttribute("value",data.sources[i].id)
                     source.innerHTML = data.sources[i].name;
                     document.getElementById('selection').appendChild(source);
-               }
-            }
+               } 
+            } 
         }  
-    })
-    $('#source').submit(function(event) {
-        event.preventDefault();
+    }) 
+    $('#source').submit(function(event) { // this grabs Source and create event listing popup
+        event.preventDefault(); // 
         alert(document.getElementById("selection").value)
-    });
-})
+    }
+    
+    // $.ajax({
+    //     url: "https://newsapi.org/v2/headlines",
+    //     data: { category:"business", country:"us", language:"en", apikey: APIKEY},
+    //     success: function(data){
+    //       if (data.status === "ok")  {
+    //           console.log(data)
+    //           for (var i = 0; i < data.sources.length; i++) { //for loop data sources
+    //                 var source = document.createElement("OPTION");
+    //                 source.setAttribute("value",data.sources.description[i].id)
+    //                 source.innerHTML = data.sources.description[i].name;
+    //                 document.getElementById('selection').appendChild(source);
+    //     })
+    );
+});
 
 
 
